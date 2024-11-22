e8edeb19c3b5b50ee0f6d2f694711b6d5b6cd407
georgebarrett <georgeaustinbarrett@gmail.com>
2024-11-22 11:32:36 +0000

    linting errors fixed

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,7 +11,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(12, 1)
 
